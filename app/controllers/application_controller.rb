bd8f6ec5e391206f881fd81b3c43c79113e1906e
Carlos Cardona <cgcardona@gmail.com>
2016-01-24 10:00:02 -0800

    Call get_coin_value on each request, formatting output, breaking views up into partials.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :get_coin_value
+    
+  def get_coin_value
+    coin_info = HTTParty.get 'http://btc.blockr.io/api/v1/coin/info' 
+    @value = coin_info['data']['markets']['coinbase']['value']
+  end
 end
