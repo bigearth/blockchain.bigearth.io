406b0b531428d15cf0f55504cd50a23366b40820
Carlos Cardona <cgcardona@gmail.com>
2016-01-27 16:24:29 -0800

    Add Social Media share buttons across the site for Twitter and Facebook.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def create_page_title 
+    if @block
+      "Bitcoin Block #{number_with_delimiter(@block['data']['nb'], delimiter: ',')} - " 
+    elsif @tx
+      "Bitcoin Transaction #{@tx['data']['tx']} - " 
+    elsif @address
+      "Bitcoin Address #{@address['data']['address']} - " 
+    end
+  end
 end
