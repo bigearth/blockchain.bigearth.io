420c1d875be21b8904261a79d270e3e7a1dd40da
Carlos Cardona <cgcardona@gmail.com>
2016-01-28 08:23:44 -0800

    Add calculate_statistics helper to explorer controller and click/tap to chance currency on homepage.

--- app/helpers/explorers_helper.rb
+++ app/helpers/explorers_helper.rb
@@ -1,2 +1,11 @@
 module ExplorersHelper
+  def calculate_statistics statistic, type
+    if statistic[0] === :size 
+      number_to_human_size statistic[1].send type
+    elsif statistic[0] === :fee
+      "#{number_with_delimiter(number_with_precision(statistic[1].send(type), precision: 8), delimiter: ',')} BTC" 
+    else
+      number_with_delimiter statistic[1].send(type), delimiter: ',' 
+    end 
+  end
 end
