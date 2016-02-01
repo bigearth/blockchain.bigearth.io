4a62239a0d7c2db9cefec6523e4efa5592b2d5b1
Carlos Cardona <cgcardona@gmail.com>
2016-02-01 11:57:29 -0800

    Precompile font assets.

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module BlockExplorer
     # config.i18n.default_locale = :de
  
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
