5a5c419ffadcb878835efe06c876e5111470a9e5
Carlos Cardona <cgcardona@gmail.com>
2016-02-02 10:20:10 -0800

    Add social media icons for share buttons.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,14 +36,4 @@ module ApplicationHelper
   def format_value value, precision
     number_with_delimiter(number_with_precision(value, precision: precision), delimiter: ',')
   end
-  
-  def create_bookmark_link 
-    unless current_page?(root_url) || current_page?(apps_calculator_path)
-      if current_page?(apps_bookmarks_path)
-        link_to 'Clear All Bookmarks', '#', class: 'btn btn-sm clear_all_bookmarks' 
-      elsif current_page?(apps_bookmarks_path) != '/apps/bookmarks'
-        link_to 'Bookmark', '#', class: 'btn btn-sm create_bookmark', "data-bookmark_type" => create_bookmark_type, "data-page_id" => params[:id] 
-      end
-    end
-  end
 end
