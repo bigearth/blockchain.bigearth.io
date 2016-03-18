ce96237f23d11ce7bb974c5228d6fd9cc1058538
Carlos Cardona <cgcardona@gmail.com>
2016-03-18 15:06:43 -0700

    Update Procfile and comment out delayed job config code.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module BlockExplorer
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
  
-    config.active_job.queue_adapter = :delayed_job
+    #config.active_job.queue_adapter = :delayed_job
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.precompile += %w( .svg .eot .woff .ttf )
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
