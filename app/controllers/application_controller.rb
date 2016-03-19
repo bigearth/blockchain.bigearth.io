23e37d999e70df39c543ea55d5094a0ffc26889e
Carlos Cardona <cgcardona@gmail.com>
2016-03-19 10:04:54 -0700

    Style breadcrumb and user registration and sign in pages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :get_coin_value
+  before_action :set_user
     
   def get_coin_value
     coin_info = HTTParty.get 'http://btc.blockr.io/api/v1/coin/info' 
     @exchange_rate = coin_info['data']['markets']['coinbase']['value']
   end
+  
+  def after_sign_in_path_for user
+    user_path user
+  end
+
+  private
+    def set_user
+      if user_signed_in?
+        @user = current_user
+      end
+    end
 end
