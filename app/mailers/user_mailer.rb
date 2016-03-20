81179c838573383f5c24216610afb5959064637c
Carlos Cardona <cgcardona@gmail.com>
2016-03-20 07:41:15 -0700

    Add authy.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,9 @@ class UserMailer < ApplicationMailer
   include SendGrid
   sendgrid_category :use_subject_lines
   def welcome_email user
-    sendgrid_category "Welcome"
+    sendgrid_category 'Welcome'
     @user = user
-    @url  = 'http://stage.blockchain.bigearth.io/users/sessions/new'
-    mail(to: @user.email, subject: 'Welcome to Big Earth!')
+    @url  = 'http://stage.blockchain.bigearth.io/users/sign_in'
+    mail to: @user.email, subject: 'Welcome to Big Earth!'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :authy_authenticatable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
   after_create :send_welcome_mail
