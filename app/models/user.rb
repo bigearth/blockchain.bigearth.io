4aa8405d19d21392261d8a43a8193aa02012ddf0
Carlos Cardona <cgcardona@gmail.com>
2016-03-20 11:56:36 -0700

    Remove methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,4 @@ class User < ActiveRecord::Base
   devise :authy_authenticatable, :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  after_create :send_confirmation_instructions
-  def send_confirmation_instructions
-    # UserMailer.confirmation_instructions(self).deliver_later
-  end
 end
