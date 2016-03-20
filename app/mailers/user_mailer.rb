684e85297a949696df108430b00ec84667f2508a
Carlos Cardona <cgcardona@gmail.com>
2016-03-20 11:47:15 -0700

    Add confirmation email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,22 +1,7 @@
 class UserMailer < Devise::Mailer
   include SendGrid
   sendgrid_category :use_subject_lines
-  
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
-  def confirmation_instructions user
-    sendgrid_category 'Welcome'
-    @user = user
-    puts "Email: #{@user.email}"
-    puts "Token: #{@user.confirmation_token}"
-    puts confirmation_url(@user, confirmation_token: @user.confirmation_token)
-    begin
-      mail to: @user.emai, subject: 'Welcome to Big Earth!'
-    rescue Exception => error
-      puts "ERROR"
-      puts "ERROR: #{error}"
-      puts error
-    end
-  end
 end
