782da8fe1c41b581ae5ce4b1dc0d53f3c766f064
Carlos Cardona <cgcardona@gmail.com>
2016-03-21 07:58:02 -0700

    Remove observer.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :authy_authenticatable, :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
+  def before_save
+    UserMailer.welcome_email if self.confirmed_at_changed?
+  end
 end
--- app/models/user_observer.rb
+++
@@ -1,5 +0,0 @@
-class UserObserver < ActiveRecord::Observer
-  def before_save
-    UserMailer.welcome_email if self.confirmed_at_changed?
-  end
-end
