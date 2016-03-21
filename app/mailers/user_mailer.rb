be56888f7a5a1a9886f01854860093e36787f44f
Carlos Cardona <cgcardona@gmail.com>
2016-03-21 15:31:28 -0700

    Add default sender.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,11 +4,14 @@ class UserMailer < Devise::Mailer
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
+  default from: Figaro.env.mailer_sender
   
   def welcome_email user
     begin
       puts "INSide WELCOME EMAIL CALLED #{user}"
-      mail(to: user.email, subject: 'Big Earth account confirmed!')
+      @user = user
+      puts "EMAIL: #{@user.email}"
+      mail(to: @user.email, subject: 'Big Earth account confirmed!')
     rescue Exception => error
       puts "ERROR: #{error}"
       puts error
