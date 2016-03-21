dc2321da21b109dd799bdee53f24e1f1049d1d11
Carlos Cardona <cgcardona@gmail.com>
2016-03-21 14:36:09 -0700

    Don't send email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,12 +13,12 @@ class UserMailer < Devise::Mailer
     puts "URL: #{@url}"
     @twofa = 'http://stageblockchain.bigearth.io/users/enable-two-factor'
     puts "TWOFA: #{@twofa}"
-    begin
-      mail(to: @user.email, subject: 'Big Earth account confirmed!')
-    rescue Exception => error
-      puts "ERROR: #{error}"
-      puts error
-      puts error.message
-    end
+    # begin
+    #   mail(to: @user.email, subject: 'Big Earth account confirmed!')
+    # rescue Exception => error
+    #   puts "ERROR: #{error}"
+    #   puts error
+    #   puts error.message
+    # end
   end
 end
