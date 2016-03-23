4de6ca390e1532af938edbca2804dba53f1f8cf7
Carlos Cardona <cgcardona@gmail.com>
2016-03-23 15:01:29 -0700

    Move user two factor auth enabled email into active::job and resque.

--- app/jobs/user_welcome_job.rb
+++ app/jobs/user_welcome_job.rb
@@ -3,7 +3,6 @@ class UserWelcomeJob < ActiveJob::Base
   queue_as :user_welcome_job
 
   def perform user
-    puts 'USER WELCOME JOB CALLED'
     UserMailer.welcome_email(user).deliver_now
   end
 end
