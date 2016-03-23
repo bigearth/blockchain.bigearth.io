35f3bfda7a46ca6bcde8db7a548d3bbc6d704faa
Carlos Cardona <cgcardona@gmail.com>
2016-03-23 14:33:42 -0700

    Add puts to debug.

--- app/jobs/user_welcome_job.rb
+++ app/jobs/user_welcome_job.rb
@@ -3,6 +3,7 @@ class UserWelcomeJob < ActiveJob::Base
   queue_as :user_welcome_job
 
   def perform user
+    puts 'USER WELCOME JOB CALLED'
     UserMailer.two_factor_auth_enabled(user).deliver_now
   end
 end
