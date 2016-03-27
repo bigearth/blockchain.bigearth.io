021c68f45e1518d2ae07e3a0f3e70de2dddb9c4c
Carlos Cardona <cgcardona@gmail.com>
2016-03-27 13:12:42 -0700

    Refactor core name of rails app.

--- config/application.rb
+++ config/application.rb
@@ -6,30 +6,32 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BlockExplorer
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+module BigEarth
+  module Blockchain
+    class Application < Rails::Application
+      # Settings in config/environments/* take precedence over those specified here.
+      # Application configuration should go into files in config/initializers
+      # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+      # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+      # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+      # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
- 
-    #config.active_job.queue_adapter = :delayed_job
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.assets.precompile += %w( .svg .eot .woff .ttf )
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+      # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+      # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+      # config.i18n.default_locale = :de
+   
+      #config.active_job.queue_adapter = :delayed_job
+      config.assets.paths << Rails.root.join("app", "assets", "fonts")
+      config.assets.precompile += %w( .svg .eot .woff .ttf )
+      config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
-    config.middleware.use Rack::Deflater
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-    config.generators do |g|
-      g.orm :active_record
+      config.middleware.use Rack::Deflater
+      # Do not swallow errors in after_commit/after_rollback callbacks.
+      config.active_record.raise_in_transactional_callbacks = true
+      config.generators do |g|
+        g.orm :active_record
+      end
     end
   end
 end
