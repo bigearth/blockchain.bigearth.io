06bfbbaf0fbee6238fc350bd33d86e3535d237bd
Carlos Cardona <cgcardona@gmail.com>
2016-03-28 11:04:31 -0700

    Use modern ruby syntax. Add minitest reporters to colorize test output. Remove useless tests.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+  
+  test 'should totally fail' do 
+    assert false
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/reporters'
+reporter_options = { color: true, slow_count: 5 }
+# Optional slow_threshold
+# reporter_options = { color: true, slow_count: 5, slow_threshold: 0.01 }
+# * https://github.com/kern/minitest-reporters
+# * http://chriskottom.com/blog/2014/06/dress-up-your-minitest-output/
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
