87e09951683753a40423b21ebba1fd16ffb96d0f
Carlos Cardona <cgcardona@gmail.com>
2016-03-28 17:42:02 -0700

    feature/tests: Comment out user and chains tests.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "the truth" do
-    assert true
-  end
+  # test "should get show" do
+  #   get :show, id: 1
+  #   assert_response :success
+  # end
 end
