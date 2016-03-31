e4cf8bf5f89d233227fbafd8c30a6545247621c3
Carlos Cardona <cgcardona@gmail.com>
2016-03-30 20:38:36 -0700

    Capture architecture brainstorm in comments.

--- app/jobs/big_earth/blockchain/bootstrap_chef_client_job.rb
+++ app/jobs/big_earth/blockchain/bootstrap_chef_client_job.rb
@@ -16,6 +16,8 @@ module BigEarth
               ipv4_address: ip_address_arr.first,
               ipv6_address: ip_address_arr.last,
               flavor: flavor 
+              # TODO instead of 'flavor' pass over an options hash w/ type and optionally flavor.
+              # Then on the other side parse the type and bootstrap chef-workstations, chef-servers and/or bitcoin nodes
             }.to_json,
             headers: { 'Content-Type' => 'application/json' } 
           )
@@ -26,3 +28,11 @@ module BigEarth
     end
   end
 end
+
+# TODO: Day Dreaming...
+# 1. Vagrant script which bootstraps local dev environment.
+#  * Sets up dev environment
+#  * Creates heroku user and application
+# 2. Additionally or independently use existing chef-server and chef-workstation to bootstrap additional chef-server/workstation(s) 
+#  * Run a rake task from the rails app which passes over the type (in this job) of node to bootstrap
+#  * It will be nice to have failsafes and redundancy in this way 
