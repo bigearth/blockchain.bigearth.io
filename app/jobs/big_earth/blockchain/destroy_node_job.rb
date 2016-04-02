13e4e77c0563457df31fa13b087fe7b26e0f87b8
Carlos Cardona <cgcardona@gmail.com>
2016-04-02 07:13:13 -0700

    Destroy node rake task w/ extensive error handling and custom exception.

--- app/jobs/big_earth/blockchain/destroy_node_job.rb
+++ app/jobs/big_earth/blockchain/destroy_node_job.rb
@@ -6,14 +6,42 @@ module BigEarth
       # Set queue
       queue_as :destroy_node_job
 
-      def perform title, email 
+      # param: config
+      #  * Hash
+      #    * type (mandatory)
+      #    * title (mandatory)
+      #    * options (optional)
+      #    * email (mandatory)
+      def perform config
+        if (config[:type].nil? || config[:type] == '') && (config[:title].nil? || config[:title] == '') && (config[:email].nil? || config[:email] == '')
+          # Missing all 3
+          raise BigEarth::Blockchain::Exceptions::CreateNodeException.new "Missing `type` and `title` and `email` (sheesh you didn't try very hard did you?)"
+        elsif (config[:title].nil? || config[:title] == '') && (config[:email].nil? || config[:email] == '')
+          # Missing title and email
+          raise BigEarth::Blockchain::Exceptions::CreateNodeException.new "Missing `title` and `email`"
+        elsif (config[:type].nil? || config[:type] == '') && (config[:email].nil? || config[:email] == '')
+          # Missing title and email
+          raise BigEarth::Blockchain::Exceptions::CreateNodeException.new "Missing `type` and `email`"
+        elsif (config[:type].nil? || config[:type] == '') && (config[:title].nil? || config[:title] == '')
+          # Missing title and email
+          raise BigEarth::Blockchain::Exceptions::CreateNodeException.new "Missing `type` and `title`"
+        elsif config[:type].nil? || config[:type] == ''
+          # Missing just type
+          raise BigEarth::Blockchain::Exceptions::CreateNodeException.new "Missing `type`"
+        elsif config[:title].nil? || config[:title] == ''
+          # Missing just title
+          raise BigEarth::Blockchain::Exceptions::CreateNodeException.new "Missing `title`"
+        elsif config[:email].nil? || config[:email] == ''
+          # Missing just title
+          raise BigEarth::Blockchain::Exceptions::CreateNodeException.new "Missing `title`"
+        end
         # Wrap in begin/rescue block
         begin
           # Get the Digital Ocean Client
           digital_ocean_client = DropletKit::Client.new access_token: Figaro.env.digital_ocean_api_token
           
           # Namespace the title by the user's email so that no global titles conflict
-          formatted_title = format_title title, email
+          formatted_title = format_title config[:title], config[:email]
               
           # select just the appropriate node
           node = fetch_node digital_ocean_client, formatted_title 
---
+++ lib/tasks/destroy_infrastructure.rake
@@ -0,0 +1,14 @@
+namespace :blockchain do
+  desc "Destroys infrastructure from the Big Earth Blockchain platform"
+  task :destroy_infrastructure, [:type, :title, :email] => :environment do |task, args|
+    # format data
+    config = {
+      type: args[:type],
+      title: args[:title],
+      email: args[:email]
+    }
+    puts "Destroying infrastructure of type: '#{config[:type]}' w/ title: '#{config[:title]}' and email: '#{config[:email]}'"
+    # Create node
+    BigEarth::Blockchain::DestroyNodeJob.perform_later config
+  end
+end
