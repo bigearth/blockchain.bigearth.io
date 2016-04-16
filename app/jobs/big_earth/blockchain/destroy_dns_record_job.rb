1a1b5dc08b7e57acac9965b27dad1b9b44c53ea6
Carlos Cardona <cgcardona@gmail.com>
2016-04-15 20:00:14 -0700

    Remove puts.

--- app/jobs/big_earth/blockchain/destroy_dns_record_job.rb
+++ app/jobs/big_earth/blockchain/destroy_dns_record_job.rb
@@ -7,7 +7,6 @@ module BigEarth
       queue_as :destroy_dns_record_job
       
       def perform config
-        puts "CONFIG: #{config}"
         # Wrap in begin/rescue block
         begin
           # Get the CloudFlare Client
@@ -18,12 +17,9 @@ module BigEarth
           
           # Namespace the title by the user's email so that no global titles conflict
           formatted_title = format_title config[:title], config[:email]
-          puts "FORMATTED TITLE: #{formatted_title}"
           
           # Loop over records
           resp['response']['recs']['objs'].each do |rec|
-            puts "****************************************************"
-            puts rec
             # Find the record for this subdomain
             if rec['name'] ==  "#{formatted_title}.bigearth.io"
               # Delete the DNS A record
