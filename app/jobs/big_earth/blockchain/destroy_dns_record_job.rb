7b3278865f17fe95b20c858e3c8c80d8b014b7f8
Carlos Cardona <cgcardona@gmail.com>
2016-04-15 19:45:43 -0700

    puts to help debug.

--- app/jobs/big_earth/blockchain/destroy_dns_record_job.rb
+++ app/jobs/big_earth/blockchain/destroy_dns_record_job.rb
@@ -7,19 +7,23 @@ module BigEarth
       queue_as :destroy_dns_record_job
       
       def perform config
+        puts "CONFIG: #{config}"
         # Wrap in begin/rescue block
         begin
           # Get the CloudFlare Client
-          cloudflare = CloudFlare::connection(Figaro.env.cloudflare_api_key, Figaro.env.cloudflare_email)
+          cloudflare = CloudFlare::connection Figaro.env.cloudflare_api_key, Figaro.env.cloudflare_email
           
           # Fetch all DNS records
           resp = cloudflare.rec_load_all Figaro.env.cloudflare_domain
           
           # Namespace the title by the user's email so that no global titles conflict
           formatted_title = format_title config['title'], config['email']
+          puts "FORMATTED TITLE: #{formatted_title}"
           
           # Loop over records
           resp['response']['recs']['objs'].each do |rec|
+            puts "****************************************************"
+            puts rec
             # Find the record for this subdomain
             if rec['name'] ==  "#{formatted_title}.bigearth.io"
               # Delete the DNS A record
