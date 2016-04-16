e48486e64354722b03259d67904868d7113198da
Carlos Cardona <cgcardona@gmail.com>
2016-04-16 10:03:10 -0700

    Update developers download count.

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -8,9 +8,16 @@ class DocsController < ApplicationController
   def developers
     client = Octokit::Client.new access_token: Figaro.env.github_token
 
+    #releases = client.releases 'bigearth/blockchain.bigearth.io'
+    repos = client.repositories 'bigearth'
+    @count = 0
+    repos.each do |repo|
+      @count += repo.forks_count
+      @count += repo.watchers_count
+      @count += repo.stargazers_count
+    end
     @blockchain_bigearth_io = client.repo 'bigearth/blockchain.bigearth.io'
     @blockchain_bigearth_io_version = client.tags 'bigearth/blockchain.bigearth.io'
-    @blockchain_bigearth_io_downloads = client.downloads 'bigearth/blockchain.bigearth.io'
     
     @chef_workstation_proxy = client.repo('bigearth/chef_workstation_proxy')
     @chef_workstation_proxy_version = client.tags 'bigearth/chef_workstation_proxy'
