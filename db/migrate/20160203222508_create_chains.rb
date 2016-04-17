0bb1ba408643882285a6f8de6ce2c456138d9bf9
Carlos Cardona <cgcardona@gmail.com>
2016-04-17 14:35:48 -0700

    Remove buttons and make default flavor core.

--- db/migrate/20160203222508_create_chains.rb
+++ db/migrate/20160203222508_create_chains.rb
@@ -3,7 +3,7 @@ class CreateChains < ActiveRecord::Migration
     create_table :chains do |t|
       t.text :pub_key
       t.string :title
-      t.string :flavor, default: 'bitcoin_core'
+      t.string :flavor, default: 'core'
       t.boolean :node_created, default: false
       t.string :ipv4_address
       t.string :ipv6_address
